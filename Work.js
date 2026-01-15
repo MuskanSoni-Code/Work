ac75de113ccf8bbbaaaae93a250d098a2c010e60
Muskan Soni <sonimuskan9451@gmail.com>
2026-01-15 21:12:15 +0530

    Add initial HTML structure with JavaScript examples

---
+++ Work.js
@@ -0,0 +1,38 @@
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <script>
+      //Easy Level
+      //question No. 1
+      let num1 = 4.67;
+      console.log(Math.round(num1));
+      //question No. 2
+      let num2 = -25;
+      console.log(Math.abs(num2));
+      //question No. 3
+      let num3 = 144;
+      console.log(Math.sqrt(num3));
+      //question No. 4
+      let num4 = Math.min(23, 56, 18, 90);
+      console.log(num4);
+      //Medium Level
+      //question No. 1
+      let num7 = 5;
+      console.log(Math.random() * 15);
+      //question No. 2
+      let num8 = Math.floor(5.8); //floor
+      console.log(num8);
+      let num9 = Math.ceil(5.8); //ceil
+      console.log(num9);
+      //question No. 3
+      let num10 = Math.pow(2, 5);
+      console.log(num10);
+      //Advanced level
+    </script>
+    let
+  </body>
+</html>
